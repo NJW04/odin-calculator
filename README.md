c2bc4905d9b3e22609a90ac432ba28fb56ffa6bb
Nathan Wells <nthnwells1@gmail.com>
2023-11-30 22:10:41 +0200

    Got Arithmetic working and outputting results to screen

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # odin-calculator
 A working calculator with working and interactive UI.
+However this calculator operates differently, as it does not take into account BEDMAS. So it will sequentially go through the entered equation, using the answer from the first sum, as the starting point for the next sum. 
+E.g. 2 + 8 ÷ 2 = 5 rather than 6 because it went, 2 + 8 = 10 first, then went 10 ÷ 2 = 5.
--- calculator.js
+++ calculator.js
@@ -1,7 +1,4 @@
 //The global variablest that hold current state
-let num1 = '';
-let num2 = '';
-let operator = '';
 let displayEquation = '';
 
 function operate(firstNum,secondNum,sign){
@@ -12,23 +9,71 @@ function operate(firstNum,secondNum,sign){
         case '-':
           return firstNum - secondNum;
           break;
-        case '*':
+        case 'x':
           return firstNum * secondNum;
           break;
-        case '/':
-            return firstNum / secondNum; //Maybe use rounding here
+        case '÷':
+            return (firstNum / secondNum).toFixed(2); //Maybe use rounding here
       }
 }
 
+function computeAnswer(stringEquation){
+  let eqnArray = stringEquation.split(' ');
+  if (eqnArray.length % 2 == 0){
+    const display = document.querySelector(".output-section");
+    display.textContent = "BAD ERROR!";
+  }
+  else{
+    console.log(eqnArray);
+    let answer;
+    for (let i=0; i<eqnArray.length-2; i += 2){
+      let num1 = Number(eqnArray[i]);
+      let sign_ = eqnArray[i+1];
+      let num2 = Number(eqnArray[i+2]);
+      answer = operate(num1,num2,sign_);
+      eqnArray[i+2] = answer;
+    }
+    return answer;
+  }
+}
+
 function updateDisplay(){
-  const currentEqn = document.querySelector('.header');
+  const currentEqn = document.querySelector('.input-section');
   currentEqn.textContent = displayEquation;
 }
 
-const allButtons = Array.from(document.querySelectorAll('.num'));
-for (const button of allButtons) {
-  button.addEventListener('click', () => {
-    displayEquation += ' ' + button.textContent;
+const allNums = Array.from(document.querySelectorAll('.num'));
+for (const num of allNums) {
+  num.addEventListener('click', () => {
+    displayEquation += num.textContent;
     updateDisplay();
   });
 }
+
+const allSigns = Array.from(document.querySelectorAll('.sign'));
+for (const sign of allSigns) {
+  sign.addEventListener('click', () => {
+    displayEquation += ' ' + sign.textContent + ' ';
+    updateDisplay();
+  });
+}
+
+const clearButton = document.querySelector('.clear');
+clearButton.addEventListener('click', () => {
+  displayEquation = '';
+  updateDisplay();
+  const outputArea = document.querySelector('.output-section');
+  outputArea.textContent = '';
+});
+
+const dotButton = document.querySelector('.dot');
+dotButton.addEventListener('click', () => {
+  displayEquation += dotButton.textContent;
+  updateDisplay();
+});
+
+const equalsButton = document.querySelector('#equals');
+equalsButton.addEventListener('click', () => {
+  const outputArea = document.querySelector('.output-section');
+  outputArea.textContent = computeAnswer(displayEquation); //Send the function the current eqn in the string for '1+2+3x2' = 12
+});
